9a7973e20979d71ec2f90b1ff8fe986f8db2a0c2
Jaden Lee <alwaysjaden@gmail.com>
2020-10-25 21:48:47 -0700

    1st stage psudocode for jacascript

---
+++ script.js
@@ -0,0 +1,29 @@
+var questions = [{ 
+        question: "1.Commony used data types DO NOT Include",
+        choices: ["string","booleans","alerts","numbers"],
+        correctAnswer:3},{
+        question: "2. The Condition is an if/else statment is enclosed within ____",
+        choices: ["quotes","bbbbb","ccccc","ddddd"],
+        correctAnswer:2},{
+        question:"3. Arrays in JavaScript can be used to store",
+        choices: ["numbers and string","other arrays","booleans","all of above"],
+        correctAnswer:3},{
+        question:"4. String Value must be enclsed within ______ when being assigned to variable",
+        choices: ["commas","curly btackets","ccccccc","dddddd"],
+        correctAnswer:3},{
+        question:"5. A very useful tool used during development and debugging for printing contents to the debugger is :",
+        choices: ["JavaScript","Terminal/bah","for loop","console.log"],
+        correctAnswer:3}
+
+
+
+]
+
+// display prompt
+//display question
+// correct or wrong
+//display timer
+function timer()
+
+// added correct values
+// prompt for Name to save score 
