3ea9c6701b8079c36f0786f9f2e8f8b2baa86d93
Jaden Lee <alwaysjaden@gmail.com>
2020-10-29 12:43:44 -0700

    displayed quiz + index matching with answer choice

--- script.js
+++ script.js
@@ -20,32 +20,33 @@ var questions = [{
     correctAnswer: 0
 }];
 
-
+// setting varible for elements and sections
 var timeLeft= document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
-var secondsLeft = 70;
+var contSec = document.querySelector("#container");
+var secondsLeft = 75;
 var timerInterval= 0;
 var questionSec = document.getElementById("questionsSec");
 var choiceSec = document.getElementById("choiceSec");
 var ulSecBuild= document.createElement("ul");
+var ulEl = document.querySelector("ul")
 var questionIndex = 0;
-
-
+var highScores = document.querySelector("#viewHighScore")
 
 // display prompt
     // Display first screen with Start Button
 // Set timer Function for the Quiz
 
 startBtn.addEventListener("click", function () {
-    // We are checking zero because its originally set to zero
+    // check for zero & set total time for the quiz
     if (timerInterval === 0) {
         timerInterval = setInterval(function () {
             secondsLeft--;
             timeLeft.textContent = "Time: " + secondsLeft;
-
+                // stop timer when it reaches 0
             if (secondsLeft <= 0) {
                 clearInterval(timerInterval);
-                allDone();
+                result();
                 alert("Time's up!");
             }
         }, 1000);
@@ -58,7 +59,7 @@ function showQuestion(questionIndex) {
     // Clears existing data 
     questionSec.innerHTML = "";
     ulSecBuild.innerHTML = "";
-    //remove Start Button
+    //remove Start Button from initial page 
     startBtn.setAttribute("style","display:none");
     // For loops to loop through all info in array
     for (var i = 0; i < questions.length; i++) {
@@ -79,7 +80,8 @@ function showQuestion(questionIndex) {
 
 function solve(event) {
     var element = event.target;
-
+    document.createElement ("hr")
+    // setting function for matching each qestion to answer array
     if (element.matches("li")) {
         var divSecBuild = document.createElement("div");
         divSecBuild.setAttribute("id","promptDiv");
@@ -87,6 +89,7 @@ function solve(event) {
         if (element.textContent === questions[questionIndex].answer){
             secondsLeft++;
             divSecBuild.textContent= "GOOD JOB You are CORRECT"
+            // penanty for worong answer 
         } else {
             secondsLeft=secondsLeft-10;
             divSecBuild.textContent= "You are WRONG";
@@ -94,27 +97,75 @@ function solve(event) {
     }
     questionIndex++
     if (questionIndex >= questions.length) {
-        // All done will append last page with user stats
+        // Finish Message and if question index reaches beyong number of index, finish the quiz and show finish message 
+        result();
         divSecBuild.textContent = "Good Job you are Done!!"
+        
     } else {
         showQuestion(questionIndex);
     }
-    questionSec.appendChild(createDiv);
+    questionSec.appendChild(divSecBuild);
 }
  
 
+function result(){
+ // Clears existing data 
+    questionSec.innerHTML = "";
+    ulSecBuild.innerHTML = "";
+// create sections for result (score )to be shown
+    if (secondsLeft>=0) {
+    var Points = secondsLeft;
+    var headingpEl = document.createElement("p");
+    clearInterval(timerInterval)
+    headingpEl.textContent = "Your Final Score is " + Points;
+    }
+ // create input section to save score and initial to local storage 
+    var headingpEl2 = document.createElement("p");
+    choiceSec.appendChild(headingpEl2);
+    headingpEl2.textContent = "Submit Your Score NOW";
 
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.setAttribute("placeholder", "Your Initial");
+    createInput.textContent = "";
 
-/*
-// sum all scores from Question 
+    choiceSec.appendChild(createInput);
+// create submit message button 
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
 
-// display total score with input name [ input in HTML]
+    choiceSec.appendChild(createSubmit);
+// give click event lister to button. 
+    createSubmit.addEventListener("click",function(){
+        var initials = createInput.value;
+            //check of error 
+        if (initials==="") {
+            console.log("NO VALUE ENTERED")
 
-// Store Name 
-submitBtn.addEventListener("click", function() {
-    // var record = name from input
-    // score from score 
-    localStorage.setItem("Name", name);
-    localStorage.setItem("score Record", totalPoint);
-  });
-*/
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: Points
+            }
+            console.log(finalScore);
+        }
+
+        // store value to local storage.
+        localStorage.setItem("user", JSON.stringify(finalScore));
+        var allScores = localStorage.getItem("allScores");
+        if (allScores ===null){
+            allScores = [];
+        } else {
+            allScores = JSON.parse(allScores);          
+        }
+        allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+        highscore()
+
+    });
+
+}   
